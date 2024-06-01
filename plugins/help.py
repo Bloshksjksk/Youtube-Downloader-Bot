bfff2e41bee70f42efdd875cfc7bd621c3a2f47d
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-06-01 09:17:59 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
 
-
+keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("𝐓𝐑𝐔𝐌𝐁𝐎𝐓𝐒", url="https://t.me/movie_time_botonly")]]
+    )
 @Client.on_message(filters.command(["help"]))
 async def start(client, message):
     helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
-    await message.reply_text(helptxt)
+    await message.reply_photo(photo="https://th.bing.com/th/id/OIG4.kIKwAP6q4rN21rOhb71Z?pid=ImgGn",caption=helptxt,reply_markup=keyboard)
